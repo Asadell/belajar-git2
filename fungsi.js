594b3dbc0a493f5f9fed9d1ff3ebc14067640320
Asadell <m.satrioasadell@gmail.com>
2024-01-19 17:04:45 +0700

    merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -4,8 +4,8 @@ console.log(add(1, 2));
 const subtract = (a, b) => a - b;
 console.log(subtract(2, 1));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 1));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 1));
 
 const divide = (a, b) => a / b;
 console.log(divide(2, 1));
